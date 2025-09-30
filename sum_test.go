7d13b0fcd85ea3fd664c963ce79dc1aea868df71
Wesley <welantoniosantana@gmail.com>
2025-09-30 17:39:09 -0300

    ci: github actions

---
+++ sum_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestSum(t *testing.T) {
+	a := 1
+	b := 2
+	expected := 3
+	result := sum(a, b)
+	if result != expected {
+		t.Errorf("Sum(%d, %d) = %d; want %d", a, b, result, expected)
+	}
+}
+
+func TestSumb(t *testing.T) {
+	a := 5
+	b := 2
+	expected := 3
+	result := sub(a, b)
+	if result != expected {
+		t.Errorf("Sum(%d, %d) = %d; want %d", a, b, result, expected)
+	}
+}
+
+func TestTimes(t *testing.T) {
+	a := 2
+	b := 2
+	expected := 4
+	result := times(a, b)
+	if result != expected {
+		t.Errorf("Times(%d, %d) = %d; want %d", a, b, result, expected)
+	}
+}
+
+func TestSumX(t *testing.T) {
+	a := 2
+	b := 2
+	expected := 6
+	result := sumX(a, b)
+	if result != expected {
+		t.Errorf("SumX(%d, %d) = %d; want %d", a, b, result, expected)
+	}
+}
