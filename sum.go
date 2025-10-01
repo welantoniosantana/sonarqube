369669ad55d74ece98d77ea1ca5376e3f2eb8c3c
Wesley <welantoniosantana@gmail.com>
2025-10-01 11:21:40 -0300

    Testado cobertura de teste - deve falhar

--- sum.go
+++ sum.go
@@ -4,9 +4,9 @@ import "fmt"
 
 func main() {
 	fmt.Println("O valor total da soma é:", sum(2, 2))
-	// fmt.Println("O valor total da subtração é:", sub(2, 2))
-	// fmt.Println("O valor total da multiplicação é:", times(2, 2))
-	// fmt.Println("O valor total da soma com dobro é:", sumX(2, 2))
+	fmt.Println("O valor total da subtração é:", sub(2, 2))
+	fmt.Println("O valor total da multiplicação é:", times(2, 2))
+	fmt.Println("O valor total da soma com dobro é:", sumX(2, 2))
 }
 
 func sum(a, b int) int {
